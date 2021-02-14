053df614491d5ee34b50a287cb4ab5aa3ed7470f
Anuj <anuj@devnos.com>
2021-02-14 11:08:50 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ async function run(): Promise<void> {
     core.info("Starting action...");
     core.debug("Starting action...");
 
+    const ms: string = core.getInput('milliseconds')
+    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
+
+    core.setOutput('time', new Date().toTimeString())
+
+
     const token = process.env['GITHUB_TOKEN'];
     // if (!token) return;
     // const octokit: github.GitHub = new github.GitHub(token)
@@ -21,6 +31,16 @@ async function run(): Promise<void> {
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+async function wait(milliseconds: number): Promise<string> {
+  return new Promise(resolve => {
+    if (isNaN(milliseconds)) {
+      throw new Error('milliseconds not a number')
+    }
+
+    setTimeout(() => resolve('done!'), milliseconds)
+  })
 }
 
 run()
